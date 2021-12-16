88b77a2e4031fd3595a937ffb1e0cd99e101ab99
Eduardo Paulini Teodoro <teodoro.edu@hotmail.com>
2021-12-16 18:02:32 -0300

    progress

--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 
 
@@ -17,7 +19,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatToolbarModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressBarModule
   ],})
 
 export class AppMaterialModule { }
